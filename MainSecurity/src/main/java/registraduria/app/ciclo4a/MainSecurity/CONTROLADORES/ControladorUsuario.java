36452e3f84f95f6328b9fe8cef8873b1214afe95
NicolasTrianaInfante <triana7@misena.edu.co>
2022-11-22 14:49:46 -0500

    Front end

--- MainSecurity/src/main/java/registraduria/app/ciclo4a/MainSecurity/CONTROLADORES/ControladorUsuario.java
+++ MainSecurity/src/main/java/registraduria/app/ciclo4a/MainSecurity/CONTROLADORES/ControladorUsuario.java
@@ -20,7 +20,6 @@ import java.util.List;
 public class ControladorUsuario {
     @Autowired
     private RepositorioUsuario miRepositorioUsuario;
-
     @Autowired
     private RepositorioRol miRepositorioRol;
 
@@ -28,26 +27,17 @@ public class ControladorUsuario {
     public List<Usuario> index(){
         return miRepositorioUsuario.findAll();
     }
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping
-    public Usuario create(@RequestBody Usuario infoUsuario){
-        infoUsuario.setContrasena(convertirSHA256(infoUsuario.getContrasena()));
-        return miRepositorioUsuario.save(infoUsuario);
-    }
+
     @GetMapping("{id}")
     public Usuario show(@PathVariable String id){
-        Usuario usuarioActual = miRepositorioUsuario
-                .findById(id)
-                .orElse(null);
+        Usuario usuarioActual = miRepositorioUsuario.findById(id).orElse(null);
         return usuarioActual;
     }
 
     @PutMapping("{id}")
     public Usuario update(@PathVariable String id, @RequestBody Usuario infoUsuario){
-        Usuario usuarioActual = miRepositorioUsuario
-                .findById(id)
-                .orElse(null);
-        if (usuarioActual!=null){
+        Usuario usuarioActual = miRepositorioUsuario.findById(id).orElse(null);
+        if(usuarioActual!=null){
             usuarioActual.setSeudonimo(infoUsuario.getSeudonimo());
             usuarioActual.setCorreo(infoUsuario.getCorreo());
             usuarioActual.setContrasena(convertirSHA256(infoUsuario.getContrasena()));
@@ -56,45 +46,52 @@ public class ControladorUsuario {
             return null;
         }
     }
-    @ResponseStatus (HttpStatus.NO_CONTENT)
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("{id}")
     public void delete(@PathVariable String id){
         Usuario usuarioActual = miRepositorioUsuario
                 .findById(id)
                 .orElse(null);
-        if (usuarioActual!= null){
+        if(usuarioActual!=null){
             miRepositorioUsuario.delete(usuarioActual);
         }
     }
 
     /**
-     *
+     * Añade un rol a un usuario
      * @param id_usuario
      * @param id_rol
-     * @return Usuario
+     * @return
      */
-    @PutMapping ("{id_usuario}/rol/{id_rol}")
-    public Usuario asignarRolAusuario(@PathVariable String id_usuario,
-                                      @PathVariable String id_rol){
+    @PutMapping("{id_usuario}/rol/{id_rol}")
+    public Usuario asignarRolUsuario(@PathVariable String id_usuario,
+                                     @PathVariable String id_rol){
         Usuario usuarioActual = miRepositorioUsuario
                 .findById(id_usuario)
                 .orElse(null);
         Rol rolActual = miRepositorioRol
                 .findById(id_rol)
                 .orElse(null);
-        if(usuarioActual!= null && rolActual != null){
+        if(usuarioActual!=null && rolActual!=null){
             usuarioActual.setRol(rolActual);
-            return  miRepositorioUsuario.save(usuarioActual);
+            return miRepositorioUsuario.save(usuarioActual);
         }else{
             return null;
         }
     }
-    @PostMapping ("/validar")
+
+    /**
+     * Validar el ingreso de algún usuario
+     * */
+    @PostMapping("/validar")
     public Usuario validate(@RequestBody Usuario infoUsuario,
-                            final HttpServletResponse response)throws IOException{
-        Usuario usuarioActual = miRepositorioUsuario.getUserByMail(infoUsuario.getCorreo());
-        if (usuarioActual != null && usuarioActual.getContrasena()
-                .equals(convertirSHA256(infoUsuario.getCorreo()))){
+                            final HttpServletResponse response)
+            throws IOException {
+        Usuario usuarioActual = miRepositorioUsuario
+                .getUserByMail(infoUsuario.getCorreo());
+        if(usuarioActual != null && usuarioActual.getContrasena()
+                .equals(convertirSHA256(infoUsuario.getContrasena()))){
             usuarioActual.setContrasena((""));
             return usuarioActual;
         }else{
@@ -102,17 +99,27 @@ public class ControladorUsuario {
             return null;
         }
     }
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Usuario create(@RequestBody Usuario infoUsuario){
+        infoUsuario.setContrasena(convertirSHA256(infoUsuario.getContrasena()));
+        return miRepositorioUsuario.save(infoUsuario);
+    }
 
+    /**
+     * Añadimos una función para manejar el encriptado de las
+     * contraseñas
+     * */
     public String convertirSHA256(String password){
         MessageDigest md = null;
-        try {
-            md =MessageDigest.getInstance("SHA-256");
+        try{
+            md = MessageDigest.getInstance("SHA-256");
         }catch (NoSuchAlgorithmException e){
             e.printStackTrace();
             return null;
         }
         byte[] hash = md.digest(password.getBytes());
-        StringBuffer sb= new StringBuffer();
+        StringBuffer sb = new StringBuffer();
         for(byte b: hash){
             sb.append(String.format("%20x", b));
         }
